bf9bb34265ca8ca4e468782a66654e9148b136fc
Rmrti <mutuma34@gmail.com>
2024-12-06 17:22:40 +0300

    Add function to find the year of the first Super Bowl win

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const superbowlWin = (record) => {
+    for (let game of record) {
+      if (game.result === "W") {
+        return game.year;
+      }
+    }
+    return undefined;
+  }
+  
